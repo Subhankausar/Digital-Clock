75d5efb079fba7677a774eeb2a907e97e6ef5b6b
Subhan Ansari <subhanansari4@gmail.com>
2024-09-03 13:28:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [time, setTime] = useState('');
+
+  useEffect(() => {
+    const timeFunc = setInterval(() => {
+      const currTime = new Date();
+      const hrs = currTime.getHours();
+      const mins = currTime.getMinutes();
+      const secs = currTime.getSeconds();
+      const ampm = hrs >= 12 ? 'PM' : 'AM';
+      
+      setTime(`${formatNumber(hrs)}:${formatNumber(mins)}:${formatNumber(secs)} sE${ampm}`);
+    }, 1000);
+
+    return () => clearInterval(timeFunc);
+  }, []);
+
+  function formatNumber(number) {
+    return number < 10 ? `0${number}` : `${number}`;
+  }
+
+  return (
+    <>
+      <h1 className='animated-h1'>{time}</h1>
+    </>
+  );
+}
+
+export default App;
+  
